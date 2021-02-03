8e7b2410117a678c4f2fc9fd3d8c1f9b5631c01b
Adithya Harsha Babu <ahb437@nyu.edu>
2021-02-02 22:34:03 -0500

    todo

---
+++ src/components/input.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./input.css";
+import useDispatch from "react-redux";
+import saveTodo from "../features/todoSlice";
+const input = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const { input, setInput } = useState("");
+  const dispatch = useDispatch();
+  const addTodo = () => {
+    dispatch(
+      saveTodo({
+        item: input,
+        done: false,
+        id: Date.now(),
+      })
+    );
+  };
+  return (
+    <div className="input">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+    </div>
+  );
+};
+
+export default input;
